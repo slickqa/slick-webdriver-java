c6b12a8846e43b533c01b634089cbe408a0f283d
Kirk Flora <kflora@sofi.org>
2017-10-09 15:35:09 -0600

    Updated interface and pom version

--- src/main/java/com/slickqa/webdriver/InFlow.java
+++ src/main/java/com/slickqa/webdriver/InFlow.java
@@ -2,37 +2,37 @@ package com.slickqa.webdriver;
 
 /**
  * The InFlow interface enforces 3 methods:
- * 1- if the browser's current page is itself
- * 2- how to handle any forms on the page
- * 3- how to continue past the page.
- * To help provide information to accomplish this when you implement this interface you will need to define the type of context object that is used to handle and complete the page.
+ * 1- if the current step is itself
+ * 2- how to handle any actions on the step
+ * 3- how to continue past the step.
+ * To help provide information to accomplish this when you implement this interface you will need to define the type of context object that is used to handle and complete the step.
  *
  * @author slambson
  */
 public interface InFlow<T> {
 
     /**
-     * Determine if the browser's current page is the one represented by this class.  It is VERY important that this
+     * Determine if the current step is the one represented by this class.  It is VERY important that this
      * be a quick check, not one that waits for 30 seconds before returning false.  You should pass in a 0 timeout to
      * whatever elements you look for in the browser.
      *
-     * @return true if the current page is the one this class represents, false otherwise.
+     * @return true if the current step is the one this class represents, false otherwise.
      */
-    boolean isCurrentPage();
+    boolean isCurrentStep();
 
     /**
-     * Handle any elements on the page, in accordance to information provided by the context object.
+     * Handle any elements on the step, in accordance to information provided by the context object.
      *
-     * @param context The context in which to handle the elements on the page.
+     * @param context The context in which to handle the elements on the step.
      * @throws Exception In case the implementing method needs to throw an exception during the handling.
      */
-    void handlePage(T context) throws Exception;
+    void handleStep(T context) throws Exception;
 
     /**
-     * Continue past page, in accordance to information provided by the context object.
+     * Continue past step, in accordance to information provided by the context object.
      *
-     * @param context The context in which to handle the elements on the page.
+     * @param context The context in which to handle the elements on the step.
      * @throws Exception In case the implementing method needs to throw an exception during the handling.
      */
-    void completePage(T context) throws Exception;
+    void completeStep(T context) throws Exception;
 }
